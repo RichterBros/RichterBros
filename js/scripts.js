06991d84eaa4b2e9224f2cc547bf0c732f656548
Erich Richter <erich.richter@gmail.com>
2020-04-07 00:34:50 -0700

    add styles html and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,50 @@
-$(document).ready(function(){
 
-});
+$(document).ready(function(){
+    
+    $("#card1").hide();
+    $("#card2").hide();   
+    
+    $("#item1").click(function( event ) {
+        //$(this).css({"background-color":"red"});
+        $("#card1").toggle();
+       
+        
+        $("#card2").hide();
+       
+           
+      
+    });   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        $("#item2").click(function( event ) {
+            $("#card2").toggle()
+       
+            $("#card1").hide();
+          
+        
+        
+        
+        });
+    
+    
+    
+       
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
